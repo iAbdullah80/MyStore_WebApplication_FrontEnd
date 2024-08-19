aa5f549ec3b6179ea0d51c2851cbb14a44bb13c3
Abdullah <itsabdullah80@gmail.com>
2024-08-19 09:33:39 +0300

    navbar changes when logged in, and basket counter

---
+++ assets/js/utils.js
@@ -0,0 +1,27 @@
+function calculateTotalPrice(cart) {
+    let totalNumberOfProducts = 0;
+    cart.forEach(item => {
+        totalNumberOfProducts += item.basketCounter;
+    });
+    const basketProducts = document.getElementById('numberInBasket');
+    basketProducts.innerHTML = totalNumberOfProducts;
+}
+if (localStorage.getItem('productsCart')){
+calculateTotalPrice(JSON.parse(localStorage.getItem('productsCart')))
+}
+
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+const tokenCookie = getCookie("access_token")
+if (tokenCookie) {
+    const dashboardBtn = document.getElementById("dashboardBtn")
+    dashboardBtn.style.display = "block"        
+}
+else {
+    const signupBtn = document.getElementById("signupBtn")
+    signupBtn.style.display = "block"
+}
