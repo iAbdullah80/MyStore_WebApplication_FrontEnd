261b9e78820127aab2cdce2b96b91ff2f59a64f6
Abdullah <itsabdullah80@gmail.com>
2024-08-21 11:37:54 +0300

    routing

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,7 +16,7 @@
     const result = await response.text();
     if (response.ok) {
         document.cookie = 'access_token=' + result + '; expires=' + new Date(Date.now() + 600000).toUTCString();
-        window.location.href = "dashboard.html"; // Redirect to index.html if the response is 200 OK
+        window.location.href = "../dashboard"; // Redirect to index.html if the response is 200 OK
         } else {
             const Failed_alert = document.getElementById("Failed_signup");
             Failed_alert.style.display = "block";
