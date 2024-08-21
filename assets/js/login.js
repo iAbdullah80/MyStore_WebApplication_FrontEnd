0b9062396c09e2155b97ddd4ca5259567637f66d
Abdullah <itsabdullah80@gmail.com>
2024-08-21 11:48:11 +0300

    ,

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,7 +15,7 @@
     const response = await fetch("https://mystore-api.alahideb.me:8443/api/user/login", requestOptions);
     const result = await response.text();
     if (response.ok) {
-        document.cookie = 'access_token=' + result + '; expires=' + new Date(Date.now() + 600000).toUTCString();
+        document.cookie = 'access_token=' + result + ', expires=' + new Date(Date.now() + 600000).toUTCString();
         window.location.href = "../dashboard"; // Redirect to index.html if the response is 200 OK
         } else {
             const Failed_alert = document.getElementById("Failed_signup");
